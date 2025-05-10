562779bf643b8450049300056e0cd652ef236856
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-10 12:32:59 +0200

    Add ListItemsUseCase to handle item listing functionality

---
+++ src/Application/UseCase/Item/ListItemsUseCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Application\UseCase\Item;
+
+use App\Application\ViewModel\Item\ItemListViewModel;
+use App\Domain\Repository\ItemRepository;
+
+final class ListItemsUseCase
+{
+    public function __construct(
+        private ItemRepository $repository,
+    ) {
+    }
+
+    public function execute(): ItemListViewModel
+    {
+        $itemList = $this->repository->findAll();
+
+        return ItemListViewModel::fromItemList($itemList);
+    }
+}
