422a9876318761bb8e78f1e1c3ea271b8dbd3757
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-10 16:12:55 +0200

    Add ItemControllerTest

--- tests/Controller/Api/ItemControllerTest.php
+++ tests/Controller/Api/ItemControllerTest.php
@@ -4,12 +4,12 @@ namespace App\Tests\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ExampleCategoryControllerTest extends WebTestCase
+class ItemControllerTest extends WebTestCase
 {
     public function testGetExampleCategories(): void
     {
         $client = static::createClient();
-        $response = $client->request('GET', '/api/example_category');
+        $response = $client->request('GET', '/api/item');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
