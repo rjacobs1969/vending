c67a32cd081134666abc5b965bd9a40e0aa33234
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-11 20:01:49 +0200

    Add CoinListViewModel and CoinViewModel: implement view models for managing and displaying coin data

---
+++ src/Application/ViewModel/Coin/CoinListViewModel.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Application\ViewModel\Coin;
+
+use App\Application\ViewModel\Coin\CoinViewModel;
+use App\Domain\Collection\CoinCollection;
+use App\Domain\ValueObject\Coin;
+
+class CoinListViewModel
+{
+    public function __construct(private array $coinViewModels)
+    {
+    }
+
+    public function toArray(): array
+    {
+        return array_map(
+            fn (CoinViewModel $coin) => $coin->toString(),
+            array_filter(
+                $this->coinViewModels,
+                fn ($coin) => $coin instanceof CoinViewModel
+            )
+        );
+    }
+
+    public function count(): int
+    {
+        return count($this->coinViewModels);
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->coinViewModels);
+    }
+
+    public function totalAmount(): string
+    {
+        $sum = (float) array_reduce(
+            $this->coinViewModels,
+            fn ($carry, CoinViewModel $coin) => $carry + (float) $coin->toString(),
+            0
+        );
+
+        return number_format(round($sum, 2), 2, '.', '');
+    }
+
+    public static function fromCoinCollection(CoinCollection $collection): self
+    {
+        $coinViewModels = array_map(
+            fn (Coin $coin) => CoinViewModel::fromCoin($coin),
+            array_filter(
+                $collection->getCoins(),
+                fn ($coin) => $coin instanceof Coin
+            )
+        );
+
+        return new self($coinViewModels);
+    }
+}
---
+++ src/Application/ViewModel/Coin/CoinViewModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Application\ViewModel\Coin;
+
+use App\Domain\ValueObject\Coin;
+
+class CoinViewModel
+{
+    public function __construct(
+        public readonly string $coinValue,
+    ) {}
+
+    public function toString(): string
+    {
+        return $this->coinValue;
+    }
+
+    public static function fromCoin(Coin $coin): self
+    {
+        return new self($coin->asCurrency());
+    }
+}
