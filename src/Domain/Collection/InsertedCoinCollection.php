37e6ff4c550441c0189b64a718475f11fb77e5a3
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-11 20:01:13 +0200

    Refactor InsertedCoinCollection: simplify class structure by extending CoinCollection and removing redundant methods

--- src/Domain/Collection/InsertedCoinCollection.php
+++ src/Domain/Collection/InsertedCoinCollection.php
@@ -4,52 +4,8 @@ namespace App\Domain\Collection;
 
 use App\Domain\ValueObject\Coin;
 
-final class InsertedCoinCollection implements \IteratorAggregate {
-    /** @var Coin[] */
-    private array $coins;
-
-    public function __construct(array $coins = []) {
-        foreach ($coins as $coin) {
-            if (!$coin instanceof Coin) {
-                throw new \InvalidArgumentException("Only Coin objects are allowed.");
-            }
-        }
-        $this->coins = $coins;
-    }
-
-    public function add(Coin $coin): void
-    {
-        $this->coins[] = $coin;
-    }
-
-    public function empty(): void
-    {
-        $this->coins = [];
-    }
-
-    public function totalAmount(): float {
-        $sum = array_reduce($this->coins, fn($sum, $coin) => $sum + $coin->asFloat(), 0);
-
-        return round($sum, 2);
-    }
-
-    public function getCoins(): array
-    {
-        return array_map(
-            fn(Coin $coin) => $coin->asCurrency(),
-            $this->coins
-        );
-    }
-
-    public function getIterator(): \Traversable {
-        return new \ArrayIterator($this->coins);
-    }
-
-    public function __toString(): string
-    {
-        return implode(", ", array_map(fn(Coin $coin) => $coin->asCurrency(), $this->coins));
-    }
-
+final class InsertedCoinCollection extends CoinCollection implements \IteratorAggregate
+{
     public function toJson(): string
     {
         return json_encode(array_map(fn(Coin $coin) => $coin->value, $this->coins));
