8dfde96483d57105d559901523b47c8e3b728cac
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-11 19:58:10 +0200

    Refactor CoinController: streamline routes and implement return coins functionality

--- src/Api/Controller/CoinController.php
+++ src/Api/Controller/CoinController.php
@@ -3,10 +3,9 @@
 namespace App\Api\Controller;
 
 use App\Api\Dto\InsertCoinDto;
-use App\Api\Dto\UpdateCoinQuantityDto;
+use App\Application\UseCase\GetTotalInsertedAmountUseCase;
 use App\Application\UseCase\InsertCoinUseCase;
-use App\Application\UseCase\ListItemsUseCase;
-use App\Application\UseCase\UpdateItemQuantityUseCase;
+use App\Application\UseCase\ReturnCoinsUseCase;
 use App\Shared\Controller\BaseController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,33 +13,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use OpenApi\Attributes as OA;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Throwable;
 
 class CoinController extends BaseController
 {
     protected $jsonContext = ['groups' => ['item:read']];
 
-    #[Route('/api/coin',
-    name: 'api_insert_coin',
-    format: 'json',
-    methods: ['POST']
-    )]
-    #[OA\Response(
-        response: Response::HTTP_OK,
-        description: 'Success'
-    )]
-    #[OA\Response(
-        response: Response::HTTP_INTERNAL_SERVER_ERROR,
-        description: 'Something went wrong'
-    )]
-    #[OA\Tag(
-        name: 'Actions'
-    )]
-    #[OA\Post(
-        description: 'Insert a coin into the vending machine',
-        operationId: 'insertCoin',
-        summary: 'Insert a coin',
-    )]
+    #[Route('/api/coin', methods: ['POST'], name: 'api_insert_coin', format: 'json')]
+    #[OA\Tag(name: 'Actions')]
+    #[OA\Response(response: Response::HTTP_OK, description: 'Success')]
+    #[OA\Response(response: Response::HTTP_UNPROCESSABLE_ENTITY,description: 'Input data validation error')]
+    #[OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR,description: 'Something went wrong')]
+    #[OA\Post(summary: 'Insert a coin', description: 'Insert a coin into the vending machine', operationId: 'insertCoin')]
     public function insertCoin(
         #[MapRequestPayload(validationGroups: ['create'])] InsertCoinDto $dto,
         InsertCoinUseCase $insertCoinUseCase,
@@ -57,87 +40,33 @@ class CoinController extends BaseController
         return $this->json(["inserted_amount" => $credit]);
     }
 
-    #[Route('/api/coin',
-        name: 'api_get_coins',
-        format: 'json',
-        methods: ['GET']
-    )]
-    #[OA\Response(
-        response: Response::HTTP_OK,
-        description: 'Success'
-    )]
-    #[OA\Response(
-        response: Response::HTTP_INTERNAL_SERVER_ERROR,
-        description: 'Something went wrong'
-    )]
-    #[OA\Tag(
-        name: 'Info'
-    )]
-    #[OA\Get(
-        description: 'List of coins available for change',
-        operationId: 'getCoins',
-        summary: 'List coins',
-    )]
-    public function getCoins(
-        ListItemsUseCase $listItemsUseCase
-    ): JsonResponse
+    #[Route('/api/coin/return', methods: ['POST'], name: 'api_return_coin', format: 'json')]
+    #[OA\Tag(name: 'Actions')]
+    #[OA\Response(response: Response::HTTP_OK, description: 'Success')]
+    #[OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong')]
+    #[OA\Post(summary: 'Return coins', description: 'Return all coins inserted into the vending machine',operationId: 'returnCoin')]
+    public function returnCoin(ReturnCoinsUseCase $returnCoinsUseCase): JsonResponse
     {
-        $items = $listItemsUseCase->execute();
+        $returnedCoins = $returnCoinsUseCase->execute();
 
-        return $this->json($items->toArray());
+        return $this->json(
+            [
+                'message' => $returnedCoins->count().' coins returned successfully',
+                'item' => null,
+                'coins' => $returnedCoins->toArray(),
+                'amount_returned' => $returnedCoins->totalAmount(),
+            ],);
     }
 
-    #[Route('/api/service/coin',
-        name: 'api_update_coin_quantity',
-        format: 'json',
-        methods: ['PUT']
-    )]
-    #[OA\Response(
-        response: Response::HTTP_OK,
-        description: 'Success'
-    )]
-    #[OA\Response(
-        response: Response::HTTP_NOT_FOUND,
-        description: 'Item not found'
-    )]
-    #[OA\Response(
-        response: Response::HTTP_UNPROCESSABLE_ENTITY,
-        description: 'Data validation error'
-    )]
-    #[OA\Response(
-        response: Response::HTTP_INTERNAL_SERVER_ERROR,
-        description: 'Something went wrong'
-    )]
-    #[OA\Tag(
-        name: 'Service'
-    )]
-    #[OA\Put(
-        description: 'Update coin quantity available for change',
-        operationId: 'updateCoinQuantity',
-        summary: 'Update coin quantity',
-    )]
-    public function updateCoinQuantity(
-        #[MapRequestPayload(validationGroups: ['updateQuantity'])] UpdateCoinQuantityDto $dto,
-        UpdateItemQuantityUseCase $updateItemQuantityUseCase,
-        ValidatorInterface $validator,
-    ): JsonResponse
+    #[Route('/api/coin/total', methods: ['GET'], name: 'api_total_coin', format: 'json')]
+    #[OA\Tag(name: 'Info')]
+    #[OA\Response(response: Response::HTTP_OK, description: 'Success')]
+    #[OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong')]
+    #[OA\Post(summary: 'Get inserted amount', description: 'Get the currently inserted coins and total amount',operationId: 'getCoin')]
+    public function viewCoin(GetTotalInsertedAmountUseCase $getTotalInsertedAmountUse): JsonResponse
     {
-        $errors = $validator->validate($dto, null, ['Default', 'updateQuantity']);
-        if (count($errors) > 0) {
-            return $this->json($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+        $credit = $getTotalInsertedAmountUse->execute();
 
-        try {
-           // $dto->setId($id);
-           // $item = $updateItemQuantityUseCase->execute($dto);
-        } catch (Throwable) {
-            return $this->json([], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-
-        return $this->json([]);
-        //$item === null ?
-        //    $this->json(['error' => 'Item with id '.$id.' not found'], Response::HTTP_NOT_FOUND) :
-        //    $this->json($item->toArray(), Response::HTTP_OK, [], $this->jsonContext);
+        return $this->json(["inserted_amount" => $credit]);
     }
-
 }
