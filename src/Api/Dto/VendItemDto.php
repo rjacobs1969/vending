59619838d93301975b263fb5d9e5d1d3a361b0f5
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-12 19:36:22 +0200

    Enhance VendItemDto validation by adding length constraints for item property

--- src/Api/Dto/VendItemDto.php
+++ src/Api/Dto/VendItemDto.php
@@ -2,11 +2,15 @@
 
 namespace App\Api\Dto;
 
+use OpenApi\Attributes as OA;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 class VendItemDto
 {
+    private const MIN_NAME_LENGTH = 3;
+    private const MAX_NAME_LENGTH = 128;
+
     public function __construct(
         #[Assert\Type(
             type: 'string',
@@ -15,6 +19,13 @@ class VendItemDto
         #[Assert\NotBlank(
             message: 'item cannot be blank'
         )]
+        #[Assert\Length(
+            min: self::MIN_NAME_LENGTH,
+            max: self::MAX_NAME_LENGTH,
+            minMessage: 'item must be at least {{ limit }} characters long',
+            maxMessage: 'item cannot be longer than {{ limit }} characters'
+        )]
+        #[OA\Property(example: "water")]
         #[Groups(["find"])]
         public readonly string $item,
     ) {
