f76505860c4b7c0750eb6796cb268713e7decf3b
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-12 13:25:12 +0200

    cleanup

--- src/Api/Controller/ItemController.php
+++ src/Api/Controller/ItemController.php
@@ -11,7 +11,6 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use OpenApi\Attributes as OA;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Throwable;
 
 class ItemController extends BaseController
@@ -21,11 +20,11 @@ class ItemController extends BaseController
     /********************************************
      * List all items with price and availability
      ********************************************/
-    #[Route('/api/item', methods: ['GET'], name: 'api_get_items', format: 'json',)]
+    #[Route('/api/item', methods: ['GET'], name: 'api_get_items', format: 'json')]
     #[OA\Response(response: Response::HTTP_OK, description: 'Success')]
-    #[OA\Response( response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong' )]
+    #[OA\Response( response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong')]
     #[OA\Tag( name: 'Info' )]
-    #[OA\Get( description: 'List of items with price and availability', operationId: 'getItems', summary: 'List items', )]
+    #[OA\Get( description: 'List of items with price and availability', operationId: 'getItems', summary: 'List items')]
     public function getItems( ListItemsUseCase $listItemsUseCase ): JsonResponse
     {
         $items = $listItemsUseCase->execute();
@@ -36,37 +35,30 @@ class ItemController extends BaseController
     /**********************
      * Update item quantity
      **********************/
-    #[Route('/api/service/item/{name}', name: 'api_update_quantity', format: 'json', methods: ['PATCH'] )]
+    #[Route('/api/service/item/{name}', name: 'api_update_quantity', format: 'json', methods: ['PATCH'])]
     #[OA\Response( response: Response::HTTP_OK, description: 'Success' )]
     #[OA\Response( response: Response::HTTP_NOT_FOUND, description: 'Item not found' )]
-    #[OA\Response( response: Response::HTTP_UNPROCESSABLE_ENTITY, description: 'Data validation error' )]
-    #[OA\Response( response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong' )]
+    #[OA\Response( response: Response::HTTP_UNPROCESSABLE_ENTITY, description: 'Data validation error')]
+    #[OA\Response( response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Something went wrong')]
     #[OA\Tag( name: 'Service' )]
-    #[OA\Patch( description: 'Update item quantity', operationId: 'updateItemQuantity', summary: 'Update item quantity', )]
+    #[OA\Patch( description: 'Update item quantity', operationId: 'updateItemQuantity', summary: 'Update item quantity')]
     public function updateItemQuantity(
         string $name,
         #[MapRequestPayload(validationGroups: ['updateQuantity'])] UpdateItemQuantityDto $dto,
         UpdateItemQuantityUseCase $updateItemQuantityUseCase,
-        ValidatorInterface $validator,
     ): JsonResponse
     {
-        $errors = $validator->validate($dto, null, ['Default']);
-        if (count($errors) > 0) {
-            return $this->json($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
         try {
-          //  $dto->setId($id);
             $dto->setName($name);
             $item = $updateItemQuantityUseCase->execute($dto);
-            if ($item === null) {
-                return $this->json( ['error' => sprintf('Item with id %d not found', $id)], Response::HTTP_NOT_FOUND );
-            }
-
-            return $this->json($item->toArray(), Response::HTTP_OK, [], $this->jsonContext);
+        } catch (\InvalidArgumentException $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_UNPROCESSABLE_ENTITY);
         } catch (Throwable) {
-            return $this->json([], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return $this->json(['error' => 'Something wend wrong'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    }
 
+        return $item === null
+                ? $this->json(['error' => sprintf('Item %s not found', $name)], Response::HTTP_NOT_FOUND)
+                : $this->json($item->toArray(), Response::HTTP_OK, [], $this->jsonContext);
+    }
 }
