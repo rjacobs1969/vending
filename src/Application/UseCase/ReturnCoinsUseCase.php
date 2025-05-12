696c43ccb8066ea2ed0dcafc6528b31465d200d3
rjacobs1969 <rjacobs@atrapalo.com>
2025-05-12 03:16:45 +0200

    Refactor ReturnCoinsUseCase to return VendViewModel and simplify constructor

--- src/Application/UseCase/ReturnCoinsUseCase.php
+++ src/Application/UseCase/ReturnCoinsUseCase.php
@@ -3,22 +3,26 @@
 namespace App\Application\UseCase;
 
 use App\Application\ViewModel\Coin\CoinListViewModel;
+use App\Application\ViewModel\Vend\VendViewModel;
 use App\Domain\Collection\ReturnCoinCollection;
 use App\Domain\Repository\TransactionRepository;
 
 final class ReturnCoinsUseCase
 {
-    public function __construct(
-        private TransactionRepository $repository,
-    ) {
+    public function __construct(private TransactionRepository $repository)
+    {
     }
 
-    public function execute(): CoinListViewModel
+    public function execute(): VendViewModel
     {
         $insertedCoinCollection = $this->repository->fetchTransaction();
         $returnCoinCollection = new ReturnCoinCollection($insertedCoinCollection->returnAllCoins());
         $this->repository->saveTransaction($insertedCoinCollection);
 
-        return CoinListViewModel::fromCoinCollection($returnCoinCollection);
+        return VendViewModel::fromCoinItemMessage(
+            CoinListViewModel::fromCoinCollection($returnCoinCollection),
+            null,
+            $returnCoinCollection->empty() ? VendViewModel::MESSAGE_NO_RETURN_COINS : VendViewModel::MESSAGE_RETURN_COINS
+        );
     }
 }
